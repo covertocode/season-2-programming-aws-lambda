6f783b5c03a18066d1ea5d7e028060173f2e9cf3
Michael <managedkaos@users.noreply.github.com>
2025-05-05 23:48:00 -0700

    add sleep

--- chapter-8-advanced-aws-lambda/ch8-scaling/hello_world/app.py
+++ chapter-8-advanced-aws-lambda/ch8-scaling/hello_world/app.py
@@ -1,10 +1,18 @@
-import json
+"""
+This is a hello world Lambda function for demonstrating Lambda scaling.
+"""
 import uuid
+import json
+import time
 
 
 def lambda_handler(event, context):
     # create a UUID for the request
     request_id = str(uuid.uuid4())
+
+    # sleep for 5000 seconds
+    time.sleep(5000)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
