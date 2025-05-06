237f033449b1ee157302295d076cbbb7534e9904
Michael <managedkaos@users.noreply.github.com>
2025-05-06 00:43:35 -0700

    add script for testing

---
+++ chapter-8-advanced-aws-lambda/ch8-scaling-java/scale-tester.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+STACK_NAME="ch8-scaling-java"
+
+API_ENDPOINT=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$STACK_NAME'][].Outputs[?OutputKey==    'ApiEndpoint'].OutputValue" --output text)
+
+while :;
+do
+    echo "$(date) $(curl -s ${API_ENDPOINT})";
+done
